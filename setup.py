6b8a1acc24537c2b8ef430e74e0312717705c36f
lukas <lukasvalentin.graf@usys.ethz.ch>
2023-03-03 10:54:58 +0100

    removing extension from setup

--- setup.py
+++ setup.py
@@ -1,31 +1,32 @@
 # File setup.py
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
-    config = Configuration('',parent_package,top_path,
-    						name = "PyPROSAIL",
-    						version = "1.0.1",
-                            description = """PyProSAIL is a Python interface to the ProSAIL combined leaf and canopy optical model.
+    config = Configuration(
+        '',
+        parent_package,top_path,
+    	name = "PyPROSAIL",
+    	version = "1.0.1",
+        description = """PyProSAIL is a Python interface to the ProSAIL combined leaf and canopy optical model.
 For more information see http://pyprosail.readthedocs.org.""",
-                            packages = ['pyprosail'],
-    						author = "Robin Wilson",
-    						author_email = "robin@rtwilson.com",
-    						url = "http://packages.python.org/PyPROSAIL",
-    						classifiers           =[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering :: Atmospheric Science",
-        "Topic :: Scientific/Engineering :: Physics",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
-        "Programming Language :: Python"
-
+        packages = ['pyprosail'],
+		author = "Robin Wilson",
+		author_email = "robin@rtwilson.com",
+		url = "http://packages.python.org/PyPROSAIL",
+		classifiers           =[
+            "Development Status :: 5 - Production/Stable",
+            "Intended Audience :: Science/Research",
+            "Topic :: Scientific/Engineering :: Atmospheric Science",
+            "Topic :: Scientific/Engineering :: Physics",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+            "Programming Language :: Python"
         ])
 
-    config.add_extension('prosail_model',
-                         sources = ['./pyprosail/MODULE_PRO4SAIL.f90', './pyprosail/dataSpec_P5B.f90',
-                         './pyprosail/LIDF.f90', './pyprosail/dladgen.f', './pyprosail/PRO4SAIL.f90',
-                         './pyprosail/prospect_5B.f90', './pyprosail/tav_abs.f90', './pyprosail/volscatt.f90',
-                         './pyprosail/PyPROSAIL.f90'])
+    # config.add_extension('prosail_model',
+    #                      sources = ['./pyprosail/MODULE_PRO4SAIL.f90', './pyprosail/dataSpec_P5B.f90',
+    #                      './pyprosail/LIDF.f90', './pyprosail/dladgen.f', './pyprosail/PRO4SAIL.f90',
+    #                      './pyprosail/prospect_5B.f90', './pyprosail/tav_abs.f90', './pyprosail/volscatt.f90',
+    #                      './pyprosail/PyPROSAIL.f90'])
     return config
 
 if __name__ == "__main__":
