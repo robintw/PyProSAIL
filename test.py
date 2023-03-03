73de20860f62f8f2fddff0eb730f35e0c63e3c6b
lukas <lukasvalentin.graf@usys.ethz.ch>
2023-03-03 10:30:18 +0100

    added optional plotting of values

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 import pyprosail
 
 res = pyprosail.run(1.5, 40, 8, 0, 0.01, 0.009, 1, 3, 0.01, 30, 0, 10, 0, pyprosail.Planophile)
-print(res)
+
+# import matplotlib.pyplot as plt
+#
+# wvl = [x for x in range(400, 2501)]
+# plt.plot(wvl, res)
+# plt.show()
