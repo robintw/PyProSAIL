7276b493013a5c7613bc9cea319aba8c8d8335b1
lukas <lukasvalentin.graf@usys.ethz.ch>
2023-03-03 13:57:36 +0100

    updating test script

--- test.py
+++ test.py
@@ -1,9 +1,25 @@
 import pyprosail
 
-res = pyprosail.run(1.5, 40, 8, 0, 0.01, 0.009, 1, 3, 0.01, 30, 0, 10, 0, pyprosail.Planophile)
+LIDFa, LIDFb = pyprosail.Planophile
+
+res = pyprosail.run(
+    N=1.5,
+    Cab=40,
+    Car=8,
+    Cbrown=0,
+    Cw=0.01,
+    Cm=0.009,
+    psoil=1,
+    LAI=3,
+    hspot=0.01,
+    tts=30,
+    tto=0,
+    psi=10,
+    TypeLidf=1,
+    LIDFa=LIDFa, 
+    LIDFb=LIDFb
+)
 
 # import matplotlib.pyplot as plt
-#
-# wvl = [x for x in range(400, 2501)]
-# plt.plot(wvl, res)
+# plt.plot(res[:,0], res[:,1])
 # plt.show()
